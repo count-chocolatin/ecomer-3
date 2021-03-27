9fd151dc21a989361457c080faf1ac87dad6729d
nombregithub <78991960+nombregithub@users.noreply.github.com>
2021-03-27 16:18:14 -0500

    user/regist

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,19 +1,23 @@
 import React, {useContext} from 'react'
 import {GlobalState} from '../../../GlobalState'
 import ProductItem from '../utils/productItem/ProductItem'
+import Loading from '../utils/loading/Loading'
 
 function Products(){
 	const state = useContext(GlobalState)
 	const [products] = state.ProductsAPI.products
-    console.log(products)
+    
 	return (
+       <>
          <div className="products">
                {
                	products.map(product =>{
                               	return <ProductItem key={product._id} product={product} />
                               })
                }
-         </div>  
-		)
+         </div> 
+         {products.length === 0 && <Loading />} 
+		    </>
+    )
 }
 export default Products
